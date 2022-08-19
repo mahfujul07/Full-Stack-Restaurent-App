828e1ddf0cdfc79fe4ac42f345469a15a7c0acad
mahfujul07 <mahfujulhuq07@gmail.com>
2022-08-20 00:05:50 +0400

    INIT

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -31,7 +31,7 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState("danger");
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  // const [{ foodItems }, dispatch] = useStateValue();
+  const [{ foodItems }, dispatch] = useStateValue();
 
   const uploadImage = (e) => {
     setIsLoading(true);
@@ -41,10 +41,10 @@ const CreateContainer = () => {
 
     uploadTask.on(
       "state_changed",
-      // (snapshot) => {
-      //   const uploadProgress =
-      //     (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      // },
+      (snapshot) => {
+        const uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      },
       (error) => {
         console.log(error);
         setFields(true);
